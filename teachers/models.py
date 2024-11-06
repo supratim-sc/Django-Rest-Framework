b8005b699b48550709a3678d10ca0de65f8305c7
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-06 18:21:05 +0530

    create teacher model

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    specalization = models.CharField(max_length=255)

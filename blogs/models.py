89958bbcf249d6fb368ab50f424ebd474b26b42f
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-06 21:50:41 +0530

    implement Blog and Comment model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+
+    def __str__(self) -> str:
+        return self.comment

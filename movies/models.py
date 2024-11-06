669fe55087362a1a64b68314186a6f2863669245
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-06 19:07:30 +0530

    create movies model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=255, unique=True)
+    released_in = models.DateField()

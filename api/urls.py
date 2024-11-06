7cfa96b4d0f3fe42b05065952036f1d0e6e98f60
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-06 11:39:39 +0530

    add library model

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('students/', include('students.urls')),
     path('employees/', include('employee.urls')),
+    path('library/', include('library.urls')),
 ]
--- library/models.py
+++ library/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Library(models.Model):
+    book_name = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    publish_year = models.DateField()

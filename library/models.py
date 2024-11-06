cca8a3f668c244ca2b967101603837b29fd88b89
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-06 12:09:22 +0530

    add unique contraint on library model

--- library/models.py
+++ library/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Library(models.Model):
-    book_name = models.CharField(max_length=255)
+    book_name = models.CharField(max_length=255, unique=True)
     author_name = models.CharField(max_length=255)
     publish_year = models.DateField()

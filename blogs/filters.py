91dd03690d666f8d0ac949120058a21c4fa5e22f
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-07 20:56:57 +0530

    implement additional filters of different types

--- blogs/filters.py
+++ blogs/filters.py
@@ -3,7 +3,9 @@ from .models import Blog
 
 class CustomFilterByBlogTitle(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name='title', lookup_expr='iexact')
+    body = django_filters.CharFilter(field_name='body', lookup_expr='icontains')
+    id = django_filters.RangeFilter(field_name='id')
 
     class Meta:
         model = Blog
-        fields = ['title']
+        fields = ['title', 'body', 'id']

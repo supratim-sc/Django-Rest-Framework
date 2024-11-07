70dbd0b8598eb7d0fed53009b8b1fa5964958346
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-07 12:15:15 +0530

    implement Custom pagination using LimitOffsetPagination

--- blogs/pagination.py
+++ blogs/pagination.py
@@ -1,4 +1,7 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 2
+
+class CustomLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 2

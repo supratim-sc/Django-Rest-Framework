023bae589f4bd2bebd841a011018b288f16720b5
Supratim Chakraborty <supratim12111996@gmail.com>
2024-11-07 22:55:44 +0530

    implemet template to easy access of endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.tempView, name='temp_page'),
     path('students/', include('students.urls')),
     path('employees/', include('employee.urls')),
     path('library/', include('library.urls')),
